924011ec7e31daea718f2141c92867b2ad16d7ea
Josh Gillies <github@joshgilli.es>
2015-04-13 17:21:46 +1000

    Update tests
    We now test the validity of the data object returned by calling `callback(err, data)`.

--- test/test-http.js
+++ test/test-http.js
@@ -1,7 +1,9 @@
+var serverTest = require('servertest')
 var webhook = require('..')
 var test = require('tape')
 var http = require('http')
 var spec = require('./spec')
+var fs = require('fs')
 
 var _webhook = webhook('/', function hook (err, data) {
   // supress tape output for err
@@ -19,6 +21,26 @@ var tests = spec(app)
 test('Vanilla HTTP', function (t) {
   Object.keys(tests).forEach(function runner (_test) {
     t.test(_test, tests[_test])
+  })
+  t.test('recieve data', function (t) {
+    var file = __dirname + '/fixtures/example.json'
+    var data = fs.readFileSync(file)
+    var rs = fs.createReadStream(file)
+    var opts = {
+      encoding: 'utf8',
+      method: 'POST',
+      headers: {
+        'x-webhook-name': 'ticket.created'
+      }
+    }
+
+    function hook (err, _data) {
+      t.ifError(err, 'no error')
+      t.deepEqual(_data, JSON.parse(data.toString()), 'original data returned')
+      t.end()
+    }
+
+    rs.pipe(serverTest(http.createServer(webhook('/', hook)), '/', opts))
   })
   t.end()
 })
